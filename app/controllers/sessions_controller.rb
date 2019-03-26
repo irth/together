ff4da181af6ceacb47483bb360750777d4512d72
Wojciech Kwolek <me@irth.pl>
2019-03-26 21:55:56 +0100

    Add a static homepage

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,6 +2,6 @@ class SessionsController < ApplicationController
 	def spotify
 		u = RSpotify::User.new(request.env['omniauth.auth'])
 		@user = User.find_or_create_spotify u
-		render plain: "Hello, #{@user.display_name}"
+		redirect_to homepage_url
 	end
 end

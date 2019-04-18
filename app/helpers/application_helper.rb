cf41b23af0abd04c17c3a5078663531878d8cebc
Wojciech Kwolek <me@irth.pl>
2019-04-18 20:43:57 +0200

    Add page titles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,9 @@
 module ApplicationHelper
-	def auth_path(provider)
-		"/auth/#{provider.to_s}"
-	end
+  def auth_path(provider)
+    "/auth/#{provider}"
+  end
+
+  def title(page_title)
+    content_for(:title) { page_title }
+  end
 end

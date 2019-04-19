17e6b83014b6014d3e7a5d09b242d170d62a5d19
Wojciech Kwolek <me@irth.pl>
2019-04-19 18:06:12 +0200

    Anonymize IPs in logs

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -1,4 +1,25 @@
 # Be sure to restart your server when you modify this file.
 
 # Configure sensitive parameters which will be filtered from the log file.
-Rails.application.config.filter_parameters += [:password]
+Rails.application.config.filter_parameters += %i[password]
+
+# anonymize IPs
+module Rails
+  module Rack
+    class Logger < ActiveSupport::LogSubscriber
+      def started_request_message(request)
+        format('Started %s "%s" for %s at %s', request.request_method, request.filtered_path, anonymized_ip(request), Time.now.to_default_s)
+      end
+
+      def anonymized_ip(request)
+        ip = IPAddr.new(request.ip)
+
+        if ip.ipv4?
+          ip.mask(24).to_s
+        else
+          ip.mask(48).to_s
+        end
+      end
+    end
+  end
+end
